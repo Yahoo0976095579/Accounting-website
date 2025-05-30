5c8dc9da93ad995e6829ee0ee2133e743c29c93b
李柏駿 <3b017012@gm.student.ncut.edu.tw>
2025-05-31 04:29:51 +0800

    優化提醒框

--- frontend/src/components/NotificationBar.vue
+++ frontend/src/components/NotificationBar.vue
@@ -1,47 +1,37 @@
-<!-- client/src/components/NotificationBar.vue -->
+<!-- Notification.vue -->
 <template>
   <transition name="fade">
     <div
-      v-if="notificationStore.isVisible"
-      :class="notificationClass"
-      class="fixed top-0 left-0 right-0 z-50 p-4 text-center text-white font-bold shadow-md"
+      v-if="notification.isVisible"
+      :class="[
+        'fixed z-50 bottom-6 right-6 min-w-[220px] max-w-xs px-4 py-3 rounded shadow-lg flex items-center',
+        notification.type === 'success' ? 'bg-green-500 text-white' : '',
+        notification.type === 'error' ? 'bg-red-500 text-white' : '',
+        notification.type === 'info' ? 'bg-blue-500 text-white' : '',
+        notification.type === 'warning' ? 'bg-yellow-400 text-black' : '',
+      ]"
+      style="pointer-events: auto"
     >
-      {{ notificationStore.message }}
+      <span class="flex-1">{{ notification.message }}</span>
+      <button class="ml-4 text-xl" @click="notification.hideNotification">
+        ×
+      </button>
     </div>
   </transition>
 </template>
 
 <script setup>
-import { computed } from "vue";
 import { useNotificationStore } from "../stores/notificationStore";
-
-const notificationStore = useNotificationStore();
-
-const notificationClass = computed(() => {
-  switch (notificationStore.type) {
-    case "success":
-      return "bg-green-500";
-    case "error":
-      return "bg-red-500";
-    case "info":
-      return "bg-blue-500";
-    case "warning":
-      return "bg-yellow-500";
-    default:
-      return "bg-gray-500";
-  }
-});
+const notification = useNotificationStore();
 </script>
 
 <style scoped>
-/* 過渡動畫 */
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.8s ease, transform 0.8s ease; /* 增加過渡時間和 transform 效果 */
+  transition: opacity 0.3s;
 }
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
-  transform: translateY(-20px); /* 從上方滑入/滑出效果 */
 }
 </style>
